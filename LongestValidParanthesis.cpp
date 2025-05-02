7a3f27952ad5214909474ee6188d3b5edcb01bf0
vineelkrish <vineelnaraboina.2017@gmail.com>
2025-05-02 15:54:28 +0530

    Completed

---
+++ LongestValidParanthesis.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+        int longestValidParentheses(string s) {
+            stack<int> st;
+            st.push(-1);
+            int maxLen=0;
+            for(int i=0;i<s.length();i++)
+            {
+                if(s[i]=='('){
+                    st.push(i);
+                }
+                else
+                {
+                    st.pop();
+                    if(st.empty())
+                    {
+                        st.push(i);
+                    }
+                    else
+                    {
+                        maxLen=max(maxLen,i-st.top());
+                    }
+                }
+            }
+            return maxLen;
+        }
+    };
