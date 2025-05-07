4e6d8cf47f602053a25abdef85a332306a568c5f
vineelkrish <vineelnaraboina.2017@gmail.com>
2025-05-07 15:18:50 +0530

    MaxStack Using Stack

---
+++ MaxStack.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+class MaxStack {
+
+    public:
+        stack<int> st;
+        stack<int> maxs;
+        MaxStack() {
+           
+        }
+        
+        void push(int val) {
+            st.push(val);
+            if(maxs.empty() || val>=maxs.top())
+            maxs.push(val);
+        }
+        
+        void pop() {
+            if(st.top() == maxs.top())
+            {
+                maxs.pop();
+            }
+            
+            st.pop();
+            
+        }
+        
+        int top() {
+            return st.top();
+        }
+        
+        int getMin() {
+            return maxs.top();
+        }
+    };
+    
+    /**
+     * Your MinStack object will be instantiated and called as such:
+     * MinStack* obj = new MinStack();
+     * obj->push(val);
+     * obj->pop();
+     * int param_3 = obj->top();
+     * int param_4 = obj->getMin();
+     */
